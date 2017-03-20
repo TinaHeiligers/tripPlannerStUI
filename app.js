a989e40d74ce3ef7f9c6741eed5de5ca675eb46e
TinaHeiligers <tina.heiligers@gmail.com>
2017-03-20 08:08:42 -0400

    server running and db synched with basic model of hotel

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var app = express();
 var morgan = require('morgan');
 var path = require('path');
 var bodyParser = require('body-parser');
-
+var nunjucks = require('nunjucks');
+var db = require('./models/db').db;
 //MIDDLEWARE
 //logging server requests
 app.use(morgan('dev'));
@@ -15,6 +16,12 @@ app.use(express.static(path.join(__dirname, '/public')));
 //parsing url and json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//templating
+nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
 
 //handling not found pages
 app.use(function(req, res, next) {
@@ -32,6 +39,12 @@ app.use(function(err, req, res, next) {
 
 //synching db and
 //starting server
-app.listen(3000, function() {
-      console.log("server is listening on port 3000");
-})
+db.sync({force: true})
+    .then(function() {
+        console.log('db has been synched....');
+    }).then(function() {
+        app.listen(3000, function() {
+          console.log("and server is listening on port 3000");
+    });
+});
+
---
+++ models/db.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var Sequelize = require("sequelize");
+var db = new Sequelize('postgres://localhost:5432/tripplannerstatic');
+
+var Hotel = db.define('hotel',{
+    name:  {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+
+    num_start: {
+      type: Sequelize.FLOAT,
+      allowNull: false
+    },
+  // amenities (string of comma delimited items)
+  // not sure about how to define this model
+    amenities: {
+      type: Sequelize.STRING,
+      allowNull: true
+      }
+});
+
+module.exports = {
+  db: db,
+  hotel: Hotel
+}
