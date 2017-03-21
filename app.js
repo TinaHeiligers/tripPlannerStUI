3653dcdbc4fcfa524f5327ce0983c41b93635cbe
TinaHeiligers <tina.heiligers@gmail.com>
2017-03-21 14:06:54 -0400

    finally have front end static UI working

--- app.js
+++ app.js
@@ -37,13 +37,6 @@ app.engine('html', nunjucks.render);
 //routes
 app.use(router);
 
-//handling not found pages
-// app.use(function(req, res, next) {
-      // var err = new Error('Not Found');
-      // err.status = 404;
-      // next(err);
-// });
-
 //handling all errors
 app.use(function(err, req, res, next) {
       res.status(err.status || 500);
--- routers/index.js
+++ routers/index.js
@@ -2,14 +2,29 @@
 
 var express = require('express');
 var router = express.Router();
-
-module.exports = router;
+var Promise = require('bluebird');
+var db = require('../models');
+var Hotel = db.model('hotel');
+var Restuarant = db.model('restaurant');
+var Activity = db.model('activity');
 
 //rendering the index page in views
 router.get('/', function(req, res, next) {
-    res.render('index');
-    next();
+    Promise.all([
+        Hotel.findAll(),
+        Restuarant.findAll(),
+        Activity.findAll()
+    ])
+    .spread(function(hotels, restuarants, activities) {
+        res.render('index', {
+            hotels: hotels,
+            restuarants: restuarants,
+            activities: activities
+        })
+    })
+    .catch(next)
 });
 
+module.exports = router;
 //rendering the index page in views
 //we can redirect to this route if there's an error loading a page.
